2b9d5ce44eae92e6f4507698d6cf969cf6fb232c
Laith15-10 <laithalshamale1@gmail.com>
2026-04-28 22:38:48 +0300

    urls.py

--- car_wash_pro/urls.py
+++ car_wash_pro/urls.py
@@ -6,7 +6,7 @@ from accounts import views # تأكد من وجود هذا السطر
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('order/', TemplateView.as_view(template_name='order.html'), name='order'),
+    path('order/', TemplateView.as_view(template_name='accounts/order.html'), name='order'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('create-order/', views.create_order, name='create_order'),
     path('driver-login/', views.driver_login, name='driver_login'),
