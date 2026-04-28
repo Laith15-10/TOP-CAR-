1b95c3ee132f46ee6dcfae40a582026c49998805
Laith15-10 <laithalshamale1@gmail.com>
2026-04-29 00:16:21 +0300

    urls.py

--- car_wash_pro/urls.py
+++ car_wash_pro/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from accounts import views # تأكد من وجود هذا السطر
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
